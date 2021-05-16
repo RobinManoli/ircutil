50aae8188688fd8ff034120e92a4589e4ba8b961
Robin Manoli <RobinManoli@users.noreply.github.com>
2021-05-16 20:27:44 +0200

    Update event.py

--- ircutil/event.py
+++ ircutil/event.py
@@ -15,6 +15,7 @@
 #	You should have received a copy of the GNU Lesser General Public License
 #	along with Ircutil.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import print_function
 import ircutil.channel
 from datetime import datetime
 
